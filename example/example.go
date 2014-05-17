ea413a798b24f1ae6fb7b08f2fe6353218e7b4b9
Uwe Hoffmann <uwe@codemanic.com>
2014-05-17 15:17:13 -0700

    initial commit

---
+++ example/example.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	sz "github.com/uwedeportivo/sevenzip"
+)
+
+var filename = flag.String("f", "example.7z", "Filename")
+
+func main() {
+	flag.Parse()
+	z, err := sz.Open(*filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Number of files in the archive: ", len(z.File))
+
+	for _, f := range z.File {
+		if f.IsDir != 0 {
+			continue
+		}
+
+		fi, err := os.Stat(f.Name)
+		if err == nil && uint64(fi.Size()) == f.Size {
+			fmt.Println("File ", f.Name, " already exists, skipping")
+			continue
+		}
+
+		fmt.Println("Extracting ", f.Name)
+		//r, err = f.Open()
+		_, err = f.OpenUnsafe()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		file := f.ReadAll()
+		/* file := make([]byte, f.Size)
+		_, err = r.Read(file)
+		if err != nil {
+			log.Fatal(err)
+		} */
+
+		dir, _ := filepath.Split(f.Name)
+		if dir != "" {
+			if err = os.MkdirAll(dir, 0755); err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		err = ioutil.WriteFile(f.Name, file, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
